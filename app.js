0d948bc17e369a16192316dd59140105c0a4f446
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-09-14 15:45:23 +0200

    ajout avatar, remodélisation du footer et ajout de couleur ainsi que du texte de présentation

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	AOS.init();
 });
+
+// $(window).scroll(function () {
+// 	console.log("ok");
+// 	$(".dComp").hide().animate({
+// 		"left": "-=100px"
+// 	}, 1);
+// 	$(".dComp").show().animate({
+// 		"left": "+=100px"
+// 	}, 1000);
+
+// });
+
+// var $window = $(window);
+// var $elem = $(".animation")
+
+// function isScrolledIntoView($elem, $window) {
+// 	var docViewTop = $window.scrollTop();
+// 	var docViewBottom = docViewTop + $window.height();
+
+// 	var elemTop = $elem.offset().top;
+// 	var elemBottom = elemTop + $elem.height();
+
+// 	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+// }
+// $(document).on("scroll", function () {
+// 	if (isScrolledIntoView($elem, $window)) {
+// 		$elem.addClass("animate")
+// 	}
+// });
